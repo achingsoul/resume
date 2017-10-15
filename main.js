21fc6cca10e5854005d232847da5b9a0fdaed4e6
mengpeiyao <345004536@qq.com>
2017-10-15 17:12:55 +0800

    提交简历

---
+++ main.js
@@ -0,0 +1,21 @@
+$(function () {
+    tab($('.nav'), $('.content'), 'click')
+
+    function tab(nav, content, event) {
+        var li = $('.nav').children()
+        li.each(function (index) {
+            $(this).on(event, function () {
+                li.eq(index).addClass('current').siblings().removeClass('current')
+                content.eq(index).addClass('active').siblings().removeClass('active')
+            })
+        })
+    }
+
+    $('#nav').on('click', 'li', function () {
+        setTimeout(function () {
+            $('.aside').removeClass('active')
+        })
+
+    })
+
+})
